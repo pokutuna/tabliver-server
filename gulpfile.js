102028341228a133c1fdf231f65cc33044d86a2d
pokutuna <popopopopokutuna@gmail.com>
2015-05-19 23:23:53 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp       = require('gulp'),
+    typescript = require('gulp-typescript');
+
+gulp.task('default', ['build', 'watch']);
+
+var tsProject = typescript.createProject({ typescript: require('typescript') });
+gulp.task('typescript', function() {
+    return gulp.src('*.ts')
+        .pipe(typescript(tsProject))
+        .js.pipe(gulp.dest('./'));
+});
+
+gulp.task('build', ['typescript']);
+
+gulp.task('watch', function() {
+    gulp.watch('./**/*.ts', ['typescript']);
+});
